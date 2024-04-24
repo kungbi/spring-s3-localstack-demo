21560d09f8781c4b6eaacdd1016f21304cfb0ccf
kungbi <kipper12335@gmail.com>
2024-04-24 18:07:05 +0900

    🔥 Remove: get image list api

--- src/main/java/com/demo/localstack_demo/controller/S3Controller.java
+++ src/main/java/com/demo/localstack_demo/controller/S3Controller.java
@@ -1,14 +1,11 @@
 package com.demo.localstack_demo.controller;
 
-import com.demo.localstack_demo.dto.UploadImageDto.GetImageListDtoRequest;
-import com.demo.localstack_demo.dto.UploadImageDto.GetImageListDtoResponse;
 import com.demo.localstack_demo.dto.UploadImageDto.UploadImageDtoRequest;
 import com.demo.localstack_demo.dto.UploadImageDto.UploadImageDtoResponse;
 import com.demo.localstack_demo.service.S3Service;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
